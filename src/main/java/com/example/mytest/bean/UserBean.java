aad0f8810571b615a58e32dad3f82510272cbf15
Your Name <hujie123@mail.ustc.edu.cn>
2021-07-01 14:16:57 +0800

    first commit

---
+++ src/main/java/com/example/mytest/bean/UserBean.java
@@ -0,0 +1,12 @@
+package com.example.mytest.bean;
+
+
+import lombok.Data;
+
+@Data
+public class UserBean {
+    private int id;
+    private String name;
+    private String password;
+    private int score;
+}
